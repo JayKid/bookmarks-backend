f4bed2116f60f197067d2cfd072bbc34fed95c7a
Josep López <cyph3rfox@gmail.com>
2022-11-19 14:09:41 +0100

    add label tables

---
+++ migrations/20221119134900_add_labels.ts
@@ -0,0 +1,14 @@
+import { Knex } from "knex";
+
+const tableName = "labels";
+exports.up = function (knex: Knex) {
+  return knex.schema.createTable(tableName, (table) => {
+    table.uuid("id").primary();
+    table.string("name").notNullable();
+    table.timestamps(true, true);
+  });
+};
+
+exports.down = function (knex: Knex) {
+  return knex.schema.dropTable(tableName);
+};
